e1cb20425dbc5c54d912a8cd4006bd8833dedd4b
Andrew Lamoureux <andrew@vector35.com>
2018-03-23 15:34:33 -0400

    stop fd mon on getline() err, fps display

--- mpw.cxx
+++ mpw.cxx
@@ -2,8 +2,12 @@
 #include <unistd.h> // STDIN_FILENO
 #include <stdint.h> // uint8_t, etc.
 #include <stdlib.h> // malloc()/free()
+#include <errno.h>
+
+#include <sys/time.h>
 
 #include <FL/Fl.H>
+#include <FL/fl_draw.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Double_Window.H>
 
@@ -20,24 +24,38 @@ class MyWin : public Fl_Double_Window
 	uint8_t *canvasData = NULL;
 	Fl_RGB_Image *canvasImage = NULL;
 
+	bool dofps = false;
+	float fps;
+	struct timespec tlast;
+	int nframes;
+
     public:
-    MyWin(int x, int y, int w_, int h_):
+    MyWin(int x, int y, int w_, int h_, bool dofps_):
         Fl_Double_Window(x, y, w_, h_)
     {
         printf("MyWin constructor\n");
+
+		dofps = dofps_;
+		if(dofps) {
+			clock_gettime(CLOCK_MONOTONIC, &tlast);
+		}
 
 		canvasData = (uint8_t *)malloc(w_*h_*3);
 		memset(canvasData, 0, w_*h_*3);
         canvasImage = new Fl_RGB_Image(canvasData, w_, h_, 3);
     }
 
-	void paint(int locX, int locY, int srcW, int srcH, uint8_t *data)
+	void insert(int locX, int locY, int srcW, int srcH, uint8_t *data)
 	{
 		int dstW = w();
 		int dstH = h();
 
+		fprintf(stderr, "locX:%d locY:%d srcW:%d srcH:%d dstW:%d dstH:%d\n",
+			locX, locY, srcW, srcH, dstW, dstH);
+
 		/* full screen replacements optimize to a memcpy */
-		if(locX==locY==0 && srcW==dstW && srcH==dstH) {
+		if((locX==0 && locY==0) && (srcW==dstW) && (srcH==dstH)) {
+			//fprintf(stderr, "optimized case\n");
 			memcpy(canvasData, data, dstW * dstH * 3);
 		}
 		else {
@@ -58,8 +76,8 @@ class MyWin : public Fl_Double_Window
 				int srcIdx = srcX + srcW * srcY;
 				int dstIdx = dstX + dstW * dstY;
 
-				printf("copying (%d,%d) -> (%d,%d) (%d bytes)\n",
-					srcX, srcY, dstX, dstY, spanX);
+				//fprintf(stderr, "copying (%d,%d) -> (%d,%d) (%d bytes)\n",
+				//	srcX, srcY, dstX, dstY, spanX);
 				memcpy(canvasData + 3*dstIdx, data + 3*srcIdx, 3*spanX);
 
 				srcY++;
@@ -125,6 +143,33 @@ class MyWin : public Fl_Double_Window
 
 		canvasImage->draw(0, 0);
 
+		if(dofps) {
+			struct timespec tnow;
+			clock_gettime(CLOCK_MONOTONIC, &tnow);
+
+			double delta = (double)(tnow.tv_nsec - tlast.tv_nsec) / 1000000000.0;
+			delta += (double)tnow.tv_sec - tlast.tv_sec;			
+			fprintf(stderr, "delta is: %f\n", delta);
+
+			/* if a second has elapsed */
+			if(delta > 1) {
+				/* record the result */
+				fps = nframes / delta;
+
+				/* restart the count */
+				tlast = tnow;
+				nframes = 0;
+			}
+			else {
+				nframes += 1;
+			}
+
+			char buf[64];
+			sprintf(buf, "FPS: %f\n", fps);
+			fl_color(0x00FF0000);
+			fl_draw(buf, 16, 16);
+		}
+
 		return;
     }
 };
@@ -133,15 +178,28 @@ void cb_fileDescrReady(FL_SOCKET fd, void *cbparam)
 {
 	uint8_t *data = NULL;
 	char *line = NULL;
-	size_t nalloc = 0;
+	size_t nalloc = 0, nchars = 0;
 	unsigned long x, y, w, h;
 	int len;
 	MyWin *win = (MyWin *)cbparam;
 
-	if(getline(&line, &nalloc, stdin) == -1) {
-		printf("ERROR getline()\n");
+
+	nchars = getline(&line, &nalloc, stdin);
+	fprintf(stderr, "getline() returned %zu\n", nchars);
+	if(nchars == -1) {
+		fprintf(stderr, "ERROR getline()\n");
+		perror("getline()");
+		fprintf(stderr, "removing it from callbacks\n");
+		Fl::remove_fd(fd);
 		goto cleanup;
 	}
+	else
+	if(nchars == 0) {
+		fprintf(stderr, "no bytes, returning\n");
+		goto cleanup;
+	}
+
+	fprintf(stderr, "getline() allocated %zu for this\n", nalloc);
 
 	len = strlen(line); // len was allocated length, not string length
 	while(line[len-1]==0x0d || line[len-1]==0x0a) {
@@ -192,24 +250,24 @@ void cb_fileDescrReady(FL_SOCKET fd, void *cbparam)
 
 		data = base64_decode(pdata, strlen(pdata));
 		if(!data) {
-			printf("ERROR base64_decode()\n");
+			fprintf(stderr, "ERROR base64_decode()\n");
 			goto cleanup;
 		}
 
-		printf("inserting at %lu,%lu size %lu,%lu data: %s\n", x,y,w,h,pdata);
-		win->paint(x, y, w, h, data);
+		//fprintf(stderr, "inserting at %lu,%lu size %lu,%lu data: %s\n", x,y,w,h,pdata);
+		win->insert(x, y, w, h, data);
 	}
 	else
 	/* SHOW command
 		 syntax: "SHOW"
 	*/
 	if(strncmp(line, "SHOW", 4) == 0) {
-		printf("showing double buffer\n");
+		fprintf(stderr, "showing double buffer\n");
 		win->redraw();
 	}
 	else
 	{
-		printf("ERROR: parsing incoming line: %s", line);
+		fprintf(stderr, "ERROR: parsing incoming line: %s", line);
 	}
 
 	cleanup:
@@ -222,17 +280,24 @@ void cb_fileDescrReady(FL_SOCKET fd, void *cbparam)
 int main(int ac, char **av) {
 	unsigned long width = SIZE_X;
 	unsigned long height = SIZE_Y;
+	bool fps = false;
 
+	/* arguments */
 	for(int i=0; i<ac; ++i) {
 		if(0==strcmp(av[i], "--width") && (i+1)<ac)
 			width = strtoul(av[i+1], NULL, 10);
 		if(0==strcmp(av[i], "--height") && (i+1)<ac)
 			height = strtoul(av[i+1], NULL, 10);
+		if(0==strcmp(av[i], "--fps"))
+			fps = true;		
 	}
+
+	/* general setup */
+    fl_font(FL_COURIER, FL_NORMAL_SIZE);
 
 	/* create the window */
 	printf("creating window sized %lux%lu\n", width, height);
-	Fl_Double_Window *win = new MyWin(POS_X, POS_Y, width, height);
+	Fl_Double_Window *win = new MyWin(POS_X, POS_Y, width, height, fps);
 	win->end();
 	win->show(0, NULL);
 
